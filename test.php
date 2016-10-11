d51ba7cbd07c72be5c6b89062d75c96c33383200
jaiyeshsidh <jaiyeshsidh@gmail.com>
2016-10-11 12:22:46 +0530

    Test File Compiled

---
+++ test.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BytePad</title>
+    <link rel="icon" href="asset/images/bytepad-logo.png">
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="asset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="asset/vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="asset/vendor/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="asset/vendor/device-mockups/device-mockups.min.css">
+
+    <!-- Theme CSS -->
+    <link href="asset/css/custom.css" rel="stylesheet">
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+
+</head>
+
+<body id="page-top">
+
+<div class="se-pre-con"></div>
+
+    <header>
+        <div class="container help-546">
+            <div class="row">
+                <div class="col-sm-6" >
+                    <div class="header-content">
+                        <div class="header-content-inner">
+                        	<div class="row">
+                        		<div class="col-lg-offset-0 col-lg-2 col-md-offset-0 col-md-3 col-sm-6 col-sm-offset-3 col-xs-offset-4 col-xs-4">
+		                        	<img src="asset/images/bytepad-logo.png" class="img-responsive center-block">
+		                        </div>
+		                        <div class="col-lg-10 col-md-9 col-sm-12 col-xs-12">
+		                        	 <h1>BYTEPAD</h1>
+		                        	 <p id="poweredbysi">Powered by SOFTWARE INCUBATOR</p>
+		                        </div>
+                        	</div>
+
+                            <h5 >Your saviour during sessionals, pre-university and university exams. Sharpen your skills with our humongous collection of question papers and solutions for all branches. Search and download any paper with ease.</h5>
+                            <h5 id="hide-in-tab">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis, tellus vitae ultricies lacinia, tellus enim posuere urna, at tincidunt eros libero sed magna. Donec vestibulum neque eu ornare laoreet. Cras sollicitudin massa at vulputate posuere. Praesent tincidunt arcu venenatis eros finibus commodo. Ut egestas accumsan dolor, vel mollis lacus finibus et. Interdum et malesuada fames ac ante ipsum primis in faucibus.</h5>  
+                            <a href="#explore" class="btn btn-outline btn-xl page-scroll center-block" id="search">EXPLORE BYTEPAD MORE ...</a>   
+                            <div class="alert alert-success fade in" id="updates">
+							    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+							    <strong class="pull-left" style="padding-right:10px; border-right: 2px solid #fe2b65;">Updates</strong> PUTs Paper are available NOW
+							</div>                       
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12 help-546" >
+                	<div class="header-search-inner" >
+                		<div id="input-holder">
+                            <input type="text" name="Search" placeholder="Enter Subject NAME or SUBJECT-CODE">
+                            <h6>Eg. Power Electronics or NAS-201 or Maths III</h6>
+                        </div>	
+                	</div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+   <div id="dark-line">
+   		<h1 id="explore"><span style="color: #fe2b65;">BytePad</span> IN EVEry exam</h1>
+   </div>
+
+    <section id="download" class="download bg-primary text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2>Discover the Power of <span>BYTEPAD</span></h2>
+                    <p>Our app is available on any mobile device! Download now to get started!</p>
+                    <hr>
+                    <div class="download-badge">
+                    	<a href="https://play.google.com/store/apps/details?id=in.silive.bo" target="_blank"><img src="asset/images/google-play-badge.svg"></a>
+                    	<img src="asset/images/app-store-badge.svg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="features" class="features">
+            <div class="container-fluid" id="features-container">
+            	<div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="section-heading">
+                        <h2>Unlimited Features, Unlimited Fun</h2>
+                        <p class="text-muted">Check out how you can Score in Exams</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            	<div class="col-sm-6 pad-off">
+            		<div class="feature-block" id="mock-bytepad">
+            			<div class="row">
+            				<h6>Mob and Web APP</h6>
+            				<h3>Download It. Anywhere.</h3>
+            				<p>Donec semper urna dui, at vehicula orci imperdiet in. Duis vel aliquam eros, et venenatis nisl. Mauris dolor arcu, mattis in lacus faucibus, vestibulum volutpat mauris. Phasellus sodales turpis quis lectus suscipit blandit ut vel sem. </p>
+            			</div>
+            			<div class="row">
+            				<!-- <img src="asset/images/mockup1.png" class="img-responsive"> -->
+            			</div>
+            		</div>
+            	</div>
+            	<div class="col-sm-6 pad-off">
+            		<div class="feature-block">
+            			<div class="row">
+            				<h6>Keywords to your Subject</h6>
+            				<h3>Search It. Read It.</h3>
+            				<p>Donec semper urna dui, at vehicula orci imperdiet in. Duis vel aliquam eros, et venenatis nisl. Mauris dolor arcu, mattis in lacus faucibus, vestibulum volutpat mauris.</p>
+            				<p class="hide-320">Donec semper urna dui, at vehicula orci imperdiet in. Duis vel aliquam eros, et venenatis nisl. Mauris dolor arcu, mattis in lacus faucibus, vestibulum volutpat mauris. Phasellus sodales turpis quis lectus suscipit blandit ut vel sem. Sed neque turpis, dapibus vel interdum nec, mollis sit amet metus. Sed ornare eleifend orci, in bibendum nulla pharetra id. Nam non leo laoreet, ornare neque id, sodales enim.</p>
+            			</div>
+            		</div>
+            	</div>
+            </div>
+    </section>
+
+
+
+	<div id="brightway">
+   		<h5></h5>
+   		<h1>Stop Hard. Start SMART.</h1>
+   		<a href="#page-top" class="btn btn-outline btn-xl page-scroll" id="bytepad-btn">BytePad It</a>
+   </div>
+
+
+<!--    <div class="container-fluid" id="social-bar">
+    <div class="row social-icon">
+        <div style="font-size: 40px;text-align: center;">
+            <h6 style="color: #fff; margin-top: 30px;">Connect Socially
+                with Software Incubator</h6>
+            <a href="https://www.facebook.com/RedefiningLimitations"
+               target="_blank"><span
+                    class="fa fa-facebook-square"></span></a>
+            <a href="https://twitter.com/si_akgec" target="_blank"><span
+                    class="fa fa-twitter-square"></span></a>
+            <a href="https://github.com/Software-Incubator"
+               target="_blank"><span
+                    class="fa fa-github-square"></span></a>
+            <a href="https://www.youtube.com/channel/UCamqzGM8e8nzsdi1bsoyaKw"
+               target="_blank"><span
+                    class="fa fa-youtube-play"></span></a>
+
+        </div>
+    </div>
+</div> -->
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2016 BYTEPAD. <img src="asset/images/silogo.svg" class="img-responsive" id="svg-logo" style="display: inline; width: 40px;"> Developed and Designed By <a id="id-link" href="http://silive.in/" target="_blank">SOFTWARE INCUBATOR.</a> </p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="asset/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="asset/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="asset/js/custom.js"></script>
+    <script type="text/javascript">
+        
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+  });
+
+    </script>
+
+</body>
+
+</html>
