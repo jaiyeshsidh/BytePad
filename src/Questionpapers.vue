c5e52372e36058ce6b6b187d7bc16eb9279654ea
Siddhant S Goral <jaiyeshsidh@gmail.com>
2017-08-13 14:43:41 +0530

    Question Paper Few Debug

--- src/Questionpapers.vue
+++ src/Questionpapers.vue
@@ -107,7 +107,7 @@ export default {
 
 <template>
   <div class="row" style="position:relative">
-    <div class="col-md-10">
+    <div class="col-md-12">
       <template v-if="totalItems">
         <ul class="list-group">
           <li class="list-group-items" v-bind:key="paper.fileName" v-for="paper in papersVisible">
@@ -136,6 +136,74 @@ export default {
 
   </div>
 </template>
+
+
+
+=========================================================================
+=========================================================================
+=========================================================================
+=========================================================================
+=========================================================================
+=========================================================================
+
+<div class="thumbnail single-list" v-if="totalItems">
+            <div class="div-info">
+                <div class="div-1">
+                    <a :href="paper.openUrl" target="_blank"><h1 v-bind:key="paper.fileName" v-for="paper in papersVisible"></h1></a>
+                    <p style="margin:0;"> Tags :
+                        <span :class="{'pdftype': pdfType(paper.fileType)}></span>
+                        <span>Lorem Ipsum</span>
+                        <span>Lorem Ipsum</span>
+                        <span>Lorem Ipsum</span>
+                    </p>
+                </div>
+                <div class="div-2">
+                    <div class="text-center" style="padding:6px; margin:0 10px; border-bottom: 2px solid #dedede;">
+                        <span class="sub-text">Exam Type :</span>
+                        <span class="text-sub">PUT</span>
+                    </div>
+                    <div class="text-center" style="padding:6px;">
+                        <span class="sub-text">Semester :</span>
+                        <span class="text-sub">Odd Semester</span>
+                    </div>
+                </div>
+                <div class="div-3">
+                    <div class="text-center" style="padding:6px; margin:0 10px; border-bottom: 2px solid #dedede;">
+                        <span class="sub-text">Session :</span>
+                        <span class="text-sub">2011 - 2012</span>
+                    </div>
+                    <div class="text-center" style="padding:6px;">
+                        <span class="sub-text">Paper Type :</span>
+                        <span class="text-sub">Answer Sheet</span>
+                    </div>
+                </div>
+            </div>
+            <div class="div-download">
+                <a :href="paper.fileUrl" :download="paper.fileUrl"><img src="asset/images/download.png" class="center-block"></a>
+                <p>125 kb</p>
+            </div>
+            <div class="div-2-3-bar">
+                <span>PUT</span>
+                <span>Odd Semester</span>
+                <span>2011 - 2012</span>
+                <span>Answer Sheet</span>
+            </div>
+        </div>
+
+
+=========================================================================
+=========================================================================
+=========================================================================
+=========================================================================
+=========================================================================
+=========================================================================
+
+
+
+
+
+
+
 
 <style media="screen">
 .pdftype a {
